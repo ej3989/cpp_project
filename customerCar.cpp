a4bb7a5511ab9e5919ad6219950b939fab3a435f
moon <ansgoals1@naver.com>
2022-10-19 15:37:01 +0900

    second change

--- customerCar.cpp
+++ customerCar.cpp
@@ -6,21 +6,17 @@
 using namespace std;
 void BFS(int x, int y,int(*visit)[100], int(*check)[100], int (*map)[100],vector<pair<int,int>> asPosition);
 void customerCar::findAs(int x, int y){
-    vector<pair<int,int>> checkAs= getAspostion();
-    int visit[100][100]={0,};
-    int check[100][100]={0,};
-    int map[100][100]={0,};
-    vector<pair<int,int>> asPosition;
-    for( auto V:checkAs){
-        asPosition.push_back(make_pair(V.first,V.second));
-    }
+    vector<pair<int,int>> checkAs= getAspostion(); //AS 센터 정보 
+    int visit[100][100]={0,}; //visit 초기화
+    int check[100][100]={0,}; //check 초기화(거리 계산용)
+    int map[100][100]={0,}; // 0으로 기본 맵 초기화
     srand(time(NULL));
-    for(int i=0;i<3000;i++){
+    for(int i=0;i<3000;i++){ //임의 랜덤으로 지도 상황 변경(1일시 교통불가)
         int newX=rand()%100;
         int newY=rand()%100;
         int flag =1;
-        for(auto X : asPosition){
-            if(X.first==newX&&X.second==newY){
+        for(auto X : checkAs){ //혹시라도 교통불가가 센터와 겹치지 않도록 설정
+            if(X.first==newX&&X.second==newY){ 
                 flag =0; break;
             }
         }
@@ -30,7 +26,7 @@ void customerCar::findAs(int x, int y){
         for(int j=0;j<10;j++) cout<<map[i][j];
         cout<<endl;
     }
-    BFS(x,y,visit,check,map,asPosition);
+    BFS(x,y,visit,check,map,checkAs);
 }
 
 
@@ -45,10 +41,10 @@ void BFS(int x, int y,int(*visit)[100], int(*check)[100], int (*map)[100],vector
         int y = q.front().second;
         q.pop();
         for(int i=0;i<4;i++){
-            int nX = x+dx[i];
-            int nY = y+dy[i];
+            int nX = x+dx[i]; //상하좌우 확인
+            int nY = y+dy[i]; //상하좌우 상황 확인
             if(0<=nX && nX<100 &&0<=nY && nY<100){
-                if(map[nX][nY]==0 && visit[nX][nY]==0){
+                if(map[nX][nY]==0 && visit[nX][nY]==0){ //map 통과 가능시, visit없을시
                     check[nX][nY]=check[x][y]+1;
                     visit[nX][nY]=1;
                     q.push(make_pair(nX,nY));
@@ -56,7 +52,7 @@ void BFS(int x, int y,int(*visit)[100], int(*check)[100], int (*map)[100],vector
             }
         }
     }
-    int MinLong = 100000;
+    int MinLong = 100000; //임의 최대치
     int MinX = asPosition[0].first;
     int MinY = asPosition[0].second;
     for(auto G : asPosition){
@@ -66,5 +62,7 @@ void BFS(int x, int y,int(*visit)[100], int(*check)[100], int (*map)[100],vector
             MinY = G.second;
             }
     }
-    cout<<"("<<MinX<<","<<MinY<<") 로 가시면 총 "<<MinLong<<"km 걸립니다."<<endl;
+    if(MinLong =100000) cout<<"기상악화로 아무곳도 갈수 없습니다"<<endl;
+    //전부 도착 못할 시 기준 가정
+    cout<<"("<<MinX<<","<<MinY<<") 로 가시면 총 "<<MinLong<<"km 걸립니다. "<<endl;
 }
--- stockmanagecar.h
+++ stockmanagecar.h
@@ -9,9 +9,10 @@ class stockManageCar: public Car{
     int quantity;
 public:
     stockManageCar(string brand="", string engine="", string carName="", 
-    string color="",string type="", int price=0, int quantity=0):Car(brand,engine,carName,color,type,price){
+    string color="",string type="", int price=0, int quantity=10):Car(brand,engine,carName,color,type,price){
         this->quantity = quantity;
     }
+    //기존 quantity는 10대로 고정.
     int getQuantity(){return quantity;}
     void quantityIncreasing(int number);
     void quantityDecreasing(int number);
