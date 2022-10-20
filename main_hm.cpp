1c52a7105bbe794bd5d8e0557007f5d1377a82ab
ej3989 <silence_taffies_0e@icloud.com>
2022-10-20 13:22:34 +0900

    main_hm.cpp 변경 전

--- main_hm.cpp
+++ main_hm.cpp
@@ -5,6 +5,7 @@
 #include "totalManager.h"
 #include "car.h"
 #include "customerCar.h"
+#include "customer.h"
 #include <iomanip>
 #include <cstring>
 using namespace std;
@@ -19,25 +20,25 @@ int main(){
     cout<<right<<setw(20)<<"홈페이지 접속을 원하시면 아무 키나 누르세요"<<endl;
     getchar();
     char choiceMenu;
+	Customer nowUser;
     while(1){
     cout<<right<<setw(20)<<"1. 일반 로그인 2. 관리자 로그인 3. 회원가입"<<endl;
+	cin >> choiceMenu;
     switch(choiceMenu){
         case '1' :
             string tempID, tempPW;
             cout<<"ID : ";
             cin>>tempID;
-            //king 클래스 리스트 소환.
-            //아이디를 확인할수 있는 함수를 만들어주세요.
+            nowUser = King.findUser(tempID);
+			//아이디를 확인할수 있는 함수를 만들어주세요.
             cout<<"PW : ";
             cin>>tempPW;
-            if(correct_psswd()){
+            if(nowUser.correct_psswd(tempID,tempPW)){
                 //brouse(); 시행
             }
             
-            
 
-
-
+			break;
         case '2' :
             string tempID, tempPW;
             cout<<"ID : ";
@@ -46,10 +47,12 @@ int main(){
             //아이디를 확인할수 있는 함수를 만들어주세요.
             cout<<"PW : ";
             cin>>tempPW;
-            correct_psswd();
+            nowUser.correct_psswd();
+			break;
 
         case '3' :
             King.join();
+			break;
 
         default :
             cout<<"잘못 입력하셨습니다"<<endl;
