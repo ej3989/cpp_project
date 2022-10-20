404cffff51460e79c8581da58971387c85b12f4c
Dal Kim <magicn05@hotmail.com>
2022-10-20 12:37:55 +0900

    added basic generator

--- customer.cpp
+++ customer.cpp
@@ -14,6 +14,7 @@ Customer::Customer(string name = "", string phone_number = "", string id = "",
   this->admin = admin;
   this->address = address;
   car_count = 0;
+  
 };
 Customer::~Customer() {}
 string Customer::getPhone() { return this->phone_number; }
